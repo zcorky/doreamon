a95af461b36ed098420df363b6b827e198945542
whatwerwant <tobewhatwewant@outlook.com>
2022-12-06 16:23:01 +0800

    chore(logger): auto add memory storage

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,12 +1,44 @@
-import { getLogger, Logger } from '@zodash/logger';
+import { Context, Middleware } from '@zodash/onion';
+import { getLogger, Logger, Options } from '@zodash/logger';
+
+const memoryStorage: Middleware<Context<any, any, any>> = async (
+  ctx,
+  next,
+) => {
+  if (!(window as any).__DOREAMON_LOGGER__._storage) {
+    (window as any).__DOREAMON_LOGGER__._storage = [];
+  } else if ((window as any).__DOREAMON_LOGGER__._storage.length > 1000) {
+    (window as any).__DOREAMON_LOGGER__._storage.shift();
+  }
+
+  (window as any).__DOREAMON_LOGGER__._storage.push(ctx.input);
+  await next();
+};
 
 const logger: Logger & {
   getLogger: typeof getLogger;
   setDisable: typeof Logger.setDisable;
-} = new Logger('Main') as any;
+} = new Logger('main') as any;
 
-logger.getLogger = getLogger;
+logger.getLogger = (name: string, options?: Options): Logger => {
+  if ((window as any).__DOREAMON_LOGGER__[name]) {
+    return (window as any).__DOREAMON_LOGGER__[name];
+  }
+
+  const lg = getLogger(name, options);
+  lg.use(memoryStorage);
+
+  if (!(window as any).__DOREAMON_LOGGER__)
+    (window as any).__DOREAMON_LOGGER__ = logger;
+  if (!(window as any).__DOREAMON_LOGGER__._namespaces)
+    (window as any).__DOREAMON_LOGGER__._namespaces = {};
+  (window as any).__DOREAMON_LOGGER__._namespaces[name] = lg;
+  return lg;
+};
+
 logger.setDisable = Logger.setDisable;
+
+logger.use(memoryStorage);
 
 if (typeof window !== 'undefined') {
   (window as any).__DOREAMON_LOGGER__ = logger;
